8cfd653df35a4d6d07dd193a448c5cdf9f6997c2
Raju Gautam <rajugautam45@gmail.com>
2018-08-08 00:04:40 +0530

    app.route() added

--- authRoutes/routes.js
+++ authRoutes/routes.js
@@ -2,15 +2,16 @@ const path = require('path');
 
 module.exports  = (app) => {
 
-  app.get('/',(req,res)=> {
+  app.route('/')
+  .get((req,res)=> {
     res.sendFile(path.join(__dirname+'/../index.html'));
+  })
+  .post((req,res) => {
+    res.send(req.body);
+    console.log(req.body);
   })
   app.post('/webhook',(req,res)=> {
     res.send(req.body);
     const check = require('../webhook/generalwebhook')(req.body);
   })
-  app.get('/webhookData',(req,res)=> {
-    res.send(req.body);
-  })
-
 }
